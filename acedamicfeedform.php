18a0c2e33b797e4d0dd6faa3170a5539a7df23b5
code with user <19228616@brookes.ac.uk>
2023-04-06 17:35:53 +0100

    commit

---
+++ acedamicfeedform.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Acedemic Feedback Form</title>
+<link href="acedamicfeedstyle.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h3>How do you consider the overall level of teaching?</h3>
+<input type="radio" id="mood-1" name=" value=Strongly Agree" class="radio">
+<label class="emoji emoji-1" for='mood3'>&#128516;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-2" name=" value=Agree" class="radio">
+<label class="emoji emoji-2" for='mood3'>&#128524;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-3" name=" value=Disagree" class="radio">
+<label class="emoji emoji-3" for='mood3'>&#128543;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-4" name=" value= Strongly Disagree" class="radio">
+<label class="emoji emoji-4" for='mood4'>&#128546;</label>&nbsp;&nbsp;&nbsp;&nbsp;<br>
+<h3>Do you consider lab activities stimulating?</h3>
+<input type="radio" id="mood-1" name=" value=Strongly Agree" class="radio">
+<label class="emoji emoji-1" for='mood3'>&#128516;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-2" name=" value=Agree" class="radio">
+<label class="emoji emoji-2" for='mood3'>&#128524;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-3" name=" value=Disagree" class="radio">
+<label class="emoji emoji-3" for='mood3'>&#128543;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-4" name=" value= Strongly Disagree' class="radio">
+<label class="emoji emoji-4" for='mood4'>&#128546;</label>&nbsp;&nbsp;&nbsp;&nbsp;<br>
+<h3>Is the coursework of this module related to the content of the module?</h3>
+<label input type="radio" id="mood-1" name=" value=Strongly Agree" class="radio">
+<label class="emoji emoji-1" for='mood3'>&#128516;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-2" name=" value=Agree" class="radio">
+<label class="emoji emoji-2" for='mood3'>&#128524;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-3" name=" value=Disagree" class="radio">
+<label class="emoji emoji-3" for='mood3'>&#128543;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-4" name=" value=Strongly Disagree' class="radio">
+<label class="emoji emoji-4" for='mood4'>&#128546;</label>&nbsp;&nbsp;&nbsp;&nbsp;<br>
+<h3>Is taught content helpful to enhance your knowledge?</h3>
+<input type="radio" id="mood-1" name=" value=Strongly Agree" class="radio">
+<label class="emoji emoji-1" for='mood3'>&#128516;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-2" name=" value=Agree" class="radio">
+<label class="emoji emoji-2" for='mood3'>&#128524;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-3" name=" value=Disagree" class="radio">
+<label class="emoji emoji-3" for='mood3'>&#128543;</label>&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" id="mood-4" name=" value= Strongly Disagree" class="radio">
+<label class="emoji emoji-4" for='mood4'>&#128546;</label>&nbsp;&nbsp;&nbsp;&nbsp;<br>
+<h3>Is there any suggestion to improve this module ?</h3>
+<textarea rows='7' cols='45'></textarea>
+<div class="feedback-btn">
+<button id="b" type="button">Submit</button>
+</div>
+</form>
+</body>
+</html>
